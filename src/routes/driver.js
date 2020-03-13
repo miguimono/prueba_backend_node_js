ae287d3d62de769901a6c5b18b45ad731232a3cf
MANS <miguimono@gmail.com>
2020-03-13 00:21:36 -0500

    se agregaron comentarios a las rutas para entenderlas mejor

--- src/routes/driver.js
+++ src/routes/driver.js
@@ -3,13 +3,41 @@ const router = Router();
 
 import { createDriver, getDriver, deleteDriver, updateDriver, getDriverById, getDriverByDate, asignDriverToOrder, releaseDriver } from "../controllers/driver";
 
+/**
+ * Crea un conductor, los parametros son:
+ * name
+ * El status del conductor siempre sera true
+ * status = true (disponible), status = false (no disponible)
+ */
 router.post('/', createDriver);
+/**
+ * Obtiene todos los conductores existentes
+ */
 router.get('/', getDriver);
+/**
+ * Obtiene un conductor segun el ID de este
+ */
 router.get('/:id', getDriverById);
+/**
+ * Elimina un conductor segun el ID de este
+ */
 router.delete('/:id', deleteDriver);
+/**
+ * Obtiene el conductor que esta realizando la entrega segun una fecha
+ */
 router.get('/date/:date', getDriverByDate);
+/**
+ * Actualiza un conductor por el ID de este
+ */
 router.put('/:id', updateDriver); 
-
+/**
+ * Asigna una orden a un conductor, el id del conductor se asigna en el parametro de la Api, y el ID de la orden en el cuerpo del mensaje
+ * El estado del conductor pasa a ser false (no disponible)
+ */
 router.put('/asignDriver/:id', asignDriverToOrder); 
+/**
+ * Elimina la orden del conductor
+ * El estado del conductor pasa a ser true (disponible)
+ */
 router.put('/releaseDriver/:id', releaseDriver); 
 export default router;
--- src/routes/order.js
+++ src/routes/order.js
@@ -3,11 +3,33 @@ const router = Router();
 
 import { createOrder, getOrder, getClient, getOrderById, deleteOrder, updateOrder } from "../controllers/order";
 
+/*Crea una nueva orden, los parametros son: 
+name,lastname,email,phone,address,date,min_hour,max_hour
+Ejemplo date: 2020-03-12
+Ejemplo min_hour: 08:00:00
+Ejemplo max_hour: 12:00:00
+La Api crea una orden, crea un cliente y una direccion
+*/
 router.post('/', createOrder);
+/**
+ * Obtiene todas las ordenes existentes
+ */
 router.get('/', getOrder);
+/**
+ * Obtiene una orden segun el ID de esta
+ */
 router.get('/:id', getOrderById);
+/**
+ * Elimina una orden segun el ID de esta
+ */
 router.delete('/:id', deleteOrder);
+/**
+ * Obtiene todos los clientes existentes
+ */
 router.get('/client', getClient);
+/**
+ * Actualiza un cliente segun ID de este
+ */
 router.put('/:id', updateOrder); 
 
 export default router;
